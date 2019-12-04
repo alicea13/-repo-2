7d8c80d398e309e48bb94293b4bc8adce45db600
alicea13 <57662851+alicea13@users.noreply.github.com>
2019-12-05 02:01:18 +0300

    Update hometask.txt.py

--- hometask.txt.py
+++ hometask.txt.py
@@ -1,2 +1,15 @@
 Ветки master и demo ничем не отличаются, так как в ветке demo у человека были
 пуговицы, шляпа и ботинки, а в ветку master мы добавили ветки hat и boots_buttons.
+
+11 коммитов:
+init
+heat added
+boots
+README added
+dots
+fix readme
+Merge remote-tracking branch 'origin/boots' into boots_buttons
+Merge remote-tracking branch 'origin/buttons' into boots_buttons
+Merge remote-tracking branch 'origin/hat'
+Merge branch 'boots_buttons'
+Создали файл hometask.txt.py
